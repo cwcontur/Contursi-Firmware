279f5ed807f5bb2eb46297174b5e367a384d10fc
cwcontur <77695646+cwcontur@users.noreply.github.com>
2021-09-28 22:28:58 -0400

    Heated Bed Timeout Increased
    Increased duration of the thermal protection periods so that the system does not call kill() when there is not a problem; it's just heating slowly.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Contursi 3D"
+#define CUSTOM_MACHINE_NAME "Contursi Cube"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -584,7 +584,7 @@
 
 // Comment the following line to disable PID and enable bang-bang.
 #define PIDTEMP
-#define BANG_MAX 255     // Limits current to nozzle while in bang-bang mode; 255=full current
+//#define BANG_MAX 255     // Limits current to nozzle while in bang-bang mode; 255=full current
 #define PID_MAX BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 
