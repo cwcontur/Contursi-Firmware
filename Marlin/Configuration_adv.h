c9010b168cc90f1ba8495b91662ab8cad15ca6d5
cwcontur <77695646+cwcontur@users.noreply.github.com>
2021-10-03 16:54:15 -0400

    Lowered XY Motor Current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2561,7 +2561,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       400        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT/4  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
@@ -2579,7 +2579,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       800
+    #define Y_CURRENT       400
     #define Y_CURRENT_HOME  Y_CURRENT/4
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
