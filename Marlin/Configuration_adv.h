b36e96d578a68111038bf0d1218461e1dc92563d
cwcontur <77695646+cwcontur@users.noreply.github.com>
2021-10-10 16:04:47 -0400

    Fixed Extruder Issue
    Switching wires fixed the issue...

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2096,7 +2096,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MINIMUM_STEPPER_PULSE 0
+#define MINIMUM_STEPPER_PULSE 1
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
