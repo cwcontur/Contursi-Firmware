6a84e55d2458a742bee1dacc222e2cee05df8665
cwcontur <77695646+cwcontur@users.noreply.github.com>
2021-10-22 17:12:40 -0400

    Serial :satellite:

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -102,7 +102,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT -1
+#define SERIAL_PORT 2
 
 /**
  * Serial Port Baud Rate
@@ -123,7 +123,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_2 1
+#define SERIAL_PORT_2 -1
 #define BAUDRATE_2 115200   // Enable to override BAUDRATE
 
 /**
