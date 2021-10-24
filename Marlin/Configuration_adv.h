4ffc9a30a36d12743b9ea81ad6714ad4fee3c88f
cwcontur <77695646+cwcontur@users.noreply.github.com>
2021-10-24 15:27:59 -0400

    X-Axis Debugging

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2592,7 +2592,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  X_CURRENT/2  // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  X_CURRENT/3  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
@@ -2610,7 +2610,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       800
-    #define Y_CURRENT_HOME  Y_CURRENT/2
+    #define Y_CURRENT_HOME  Y_CURRENT/4
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
